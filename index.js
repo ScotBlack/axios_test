a8c7815ee4c55a32294977ae15c3cbcd17ae296b
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-03-20 15:19:50 +0100

    make request to api in async func

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-console.log("test")
+
+
+function getPlayers() {
+    axios
+        .get('http://localhost:8080/players', {
+            timeout: 5000
+        })
+        .then(res => console.log(res.data))
+        .catch(err=> console.error(err))
+}
+
+
+// getPlayers()
+
+async function getPlayersTwo () {
+    const response =  await axios.get('http://localhost:8080/players');
+
+    console.log(response);
+}
+
+getPlayersTwo();
