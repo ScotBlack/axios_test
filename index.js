321cfe9fec06effac48d33385692043404a1bd9d
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-03-20 15:26:24 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 
 
-
-function getPlayers() {
-    axios
-        .get('http://localhost:8080/players', {
-            timeout: 5000
-        })
-        .then(res => console.log(res.data))
-        .catch(err=> console.error(err))
-}
-
-
 // getPlayers()
 
 async function getPlayers () {
     const response =  await axios.get('http://localhost:8080/players');
 
     const playersList = document.getElementById('players')
+
     console.log(response);
     console.log(playersList)
 }
