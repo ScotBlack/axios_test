383eb5cbee0a30dcfb7036c3d103f93aeeb24ef7
Ian Hupkes <ScotBlack@users.noreply.github.com>
2021-03-20 15:23:52 +0100

    make request to api in async func

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ function getPlayers() {
 
 // getPlayers()
 
-async function getPlayersTwo () {
+async function getPlayers () {
     const response =  await axios.get('http://localhost:8080/players');
 
+    const playersList = document.getElementById('players')
     console.log(response);
+    console.log(playersList)
 }
 
-getPlayersTwo();
+getPlayers();
